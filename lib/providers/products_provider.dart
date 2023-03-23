73ef0402eb80ade7f89b278acd047b27e0d9a8f3
YehorAltShuler <107608882+YehorAltShuler@users.noreply.github.com>
2023-03-23 16:28:35 +0300

    Favorites feature & screen added

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -14,6 +14,7 @@ class Products with ChangeNotifier {
       rate: 4.5,
       imageUrl:
           'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2015_40/796316/latteartheart-11-150929.jpg',
+      isFavorite: true,
     ),
     Product(
       id: 'p2',
@@ -25,6 +26,7 @@ class Products with ChangeNotifier {
       rate: 4.5,
       imageUrl:
           'https://cdn.pixabay.com/photo/2021/02/03/12/00/coffee-5977682_1280.jpg',
+      isFavorite: false,
     ),
     Product(
       id: 'p3',
@@ -36,6 +38,7 @@ class Products with ChangeNotifier {
       rate: 4.5,
       imageUrl:
           'https://i.pinimg.com/originals/d2/20/cc/d220cc7597866bd990146ee2255e3672.jpg',
+      isFavorite: false,
     ),
     Product(
       id: 'p4',
@@ -47,28 +50,7 @@ class Products with ChangeNotifier {
       rate: 4.5,
       imageUrl:
           'https://www.justonecookbook.com/wp-content/uploads/2022/12/Matcha-Latte-4598-I-1.jpg',
-    ),
-    Product(
-      id: 'p3',
-      drinkType: 'Cappuchino',
-      title: 'Bursting Blueberry',
-      description:
-          'A single espresso shot poured into hot foamy milk, with the surface topped with mildly sweetened cocoa powder and drizzled with scrumptious caramel syrup',
-      price: 3.20,
-      rate: 4.5,
-      imageUrl:
-          'https://i.pinimg.com/originals/d2/20/cc/d220cc7597866bd990146ee2255e3672.jpg',
-    ),
-    Product(
-      id: 'p4',
-      drinkType: 'Cappuchino',
-      title: 'Dalgona Whipped Macha',
-      description:
-          'A single espresso shot poured into hot foamy milk, with the surface topped with mildly sweetened cocoa powder and drizzled with scrumptious caramel syrup',
-      price: 4.20,
-      rate: 4.5,
-      imageUrl:
-          'https://www.justonecookbook.com/wp-content/uploads/2022/12/Matcha-Latte-4598-I-1.jpg',
+      isFavorite: false,
     ),
   ];
 
@@ -82,8 +64,13 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  List<Product> get favoriteItems {
-    return _items.where((prodItem) => prodItem.isFavorite).toList();
+  void toggleFavorite(Product product) {
+    product.isFavorite = !product.isFavorite;
+    notifyListeners();
+  }
+
+  List<Product> get favoriteProducts {
+    return _items.where((prodItem) => prodItem.isFavorite == true).toList();
   }
 
   void filterProductList(List<Product> productList) {
